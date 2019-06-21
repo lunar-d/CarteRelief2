16f2d30a3492ad9896aeab281c9d8f0e94838da4
giaccone <giaccone.r@gmail.com>
2019-06-22 01:39:01 +0200

    connection série
    instalation de johnny-five et serialport

--- assets/js/arduino.js
+++ assets/js/arduino.js
@@ -1,5 +1,19 @@
 const SerialPort = require('serialport');
-var arduinoCOMPort = "COM11";
+var arduinoCOMPort = "COM3";
 const arduinoSerialPort = new SerialPort(arduinoCOMPort, {  
   baudRate: 9600
  });
+ arduinoSerialPort.on('open',function() {
+  console.log('Serial Port ' + arduinoCOMPort + ' is opened.');
+});
+
+/* var five = require("johnny-five"),
+    board = new five.Board();
+
+board.on("ready", function() {
+  // Create an Led on pin 13
+  var led = new five.Led(13);
+
+  // Strobe the pin on/off, defaults to 100ms phases
+  led.strobe();
+});*/
